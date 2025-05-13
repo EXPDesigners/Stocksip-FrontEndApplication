9e85ee0679d6b08c8f4eb24e055db88c6bdab229
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-05-13 17:02:09 -0500

    feat(product): add product entity.

---
+++ src/inventory-management/model/product.entity.js
@@ -0,0 +1,41 @@
+import {generateUuid} from "../../shared/model/uuid.js";
+import {ValidationError} from "../../shared/model/errors.js";
+
+export class Product {
+    constructor({
+                    name='',
+                    unitPrice=0,
+                    content=0,
+                    expirationDate= null,
+                    imageUrl='',
+                    productType='',
+                    currentStock=0,
+                    minimumStock=0,
+                    providerId=''
+                })
+    {
+        if (!Number.isInteger(currentStock) || currentStock <= 0) {
+            throw ValidationError('Current stock of product must be a positive integer number.')
+        }
+
+        if (!Number.isInteger(minimumStock) || minimumStock <= 0) {
+            throw ValidationError('Minimum stock of product must be a positive integer number.')
+        }
+
+        if (content <= 0) {
+            throw ValidationError('Content of product must be a positive number.')
+        }
+
+        this.id = generateUuid();
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.content = content;
+        this.expirationDate = expirationDate;
+        this.imageUrl = imageUrl;
+        this.productType = productType;
+        this.currentStock = currentStock;
+        this.minimumStock = minimumStock;
+        this.state = "IN-INVENTORY";
+        this.providerId = providerId;
+    }
+}
