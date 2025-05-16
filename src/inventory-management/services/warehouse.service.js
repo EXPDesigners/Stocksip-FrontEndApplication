f12f5916f067f6dc0e6cf2ec52f097853cd45a48
Farid Coronel <faridce14@gmail.com>
2025-05-16 04:46:41 -0500

    feat(public): add warehouse service.

--- src/inventory-management/services/warehouse.service.js
+++ src/inventory-management/services/warehouse.service.js
@@ -1,4 +1,5 @@
 import {BaseService} from "../../shared/services/base.service.js";
+import {generateUuid} from "@/shared/model/uuid.js";
 
 /**
  * @class WarehouseService
@@ -17,6 +18,55 @@ export class WarehouseService extends BaseService {
      */
     async getWarehouses() {
         const response = await this.getAll();
+        return response.data;
+    }
+
+    /**
+     * Get a warehouse by ID
+     * @param id
+     * @returns {Promise<Object>} The warehouse data
+     */
+    async getWarehouseById(id) {
+        const response = await this.getById(id);
+        return response.data;
+    }
+
+    /**
+     * Update a warehouse
+     * @param warehouse
+     * @returns {Promise<Object>} Created warehouse data
+     */
+    async createWarehouse(warehouse) {
+        const adaptedWarehouse = {
+            id: warehouse.id || generateUuid(),
+            ...warehouse
+        };
+        const response = await this.create(adaptedWarehouse);
+        return response.data;
+    }
+
+    /**
+     * Delete a warehouse
+     * @param {string} id - The ID of the warehouse to delete
+     * @returns {Promise<Object>} The deleted warehouse data
+     */
+    async deleteWarehouse(id) {
+        const response = await this.delete(id);
+        return response.data;
+    }
+
+    /**
+     * Update a warehouse
+     * @param {string} id - The ID of the warehouse to update
+     * @param {Object} warehouse - The updated warehouse data
+     * @returns {Promise<Object>} The updated warehouse data
+     */
+    async updateWarehouse(id, warehouse) {
+        const warehouseToUpdate = {
+            ...warehouse,
+            id: id
+        };
+        const response = await this.update(id, warehouseToUpdate);
         return response.data;
     }
 }
